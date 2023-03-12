b3b24a7989355abeabf19b55dbd4a3e7a5986eab
Bren T <paxtbiz@gmail.com>
2023-03-12 14:28:04 -0700

    updated server

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 
 
-
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001
+
+
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+
+
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+console.log(`app listening at http://localhost:${PORT}`))
